7d5212c74f425710f6f22262ae980048e70c9d32
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-28 22:18:57 +0900

    add prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import prisma from '../lib/prisma';
+
+const userId = 'auth0|000000000000000000000000';
+const main = async () => {
+  for (let i = 0; i < 100; i++)
+    await prisma.timerRecord.create({
+      data: { time: Math.trunc(Math.random() * 10000), userId },
+    });
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
