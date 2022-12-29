5af3c2280edad00922f9e48f95da7603afcffcc4
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-29 15:54:30 +0900

    fix type error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,12 @@ const userId = 'auth0|000000000000000000000000';
 const main = async () => {
   for (let i = 0; i < 100; i++)
     await prisma.timerRecord.create({
-      data: { time: Math.trunc(Math.random() * 10000), userId },
+      data: {
+        time: Math.trunc(Math.random() * 10000),
+        penalty: false,
+        dnf: false,
+        userId,
+      },
     });
 };
 
