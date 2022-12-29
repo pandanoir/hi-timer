3af8d28622bd1941f3f808db8b13594f932cf342
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-29 16:14:08 +0900

    remove styles

--- app/(timer)/page.tsx
+++ app/(timer)/page.tsx
@@ -2,7 +2,6 @@
 import { UserProfile, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { FC, useState } from 'react';
 import useSWR, { useSWRConfig } from 'swr';
-import styles from '../../styles/Home.module.css';
 import { Timer } from './components/Timer';
 
 type TimerRecord = {
@@ -115,9 +114,9 @@ const TimerPage: FC<{ user: UserProfile }> = () => {
   } = useTimerRecords();
   const [isTimerRecording, setIsTimerRecording] = useState(false);
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hi Timer</h1>
+    <div>
+      <main>
+        <h1>Hi Timer</h1>
         {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
         <a href="/api/auth/logout">Logout</a>
         uses inspection:
--- app/top/page.tsx
+++ app/top/page.tsx
@@ -1,9 +1,7 @@
-import styles from '../../styles/Home.module.css';
-
 const TopPage = () => (
-  <div className={styles.container}>
-    <main className={styles.main}>
-      <h1 className={styles.title}>Hi Timer</h1>
+  <div>
+    <main>
+      <h1>Hi Timer</h1>
       {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       <a href="/api/auth/login">login</a>
     </main>
