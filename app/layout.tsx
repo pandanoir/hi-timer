b8e5b9ea71a4e5b5d99ea385de77958b751bdc97
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-31 15:34:52 +0900

    improve performance of SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,34 @@
 import { PropsWithChildren } from 'react';
-import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
-const getUser = async () =>
-  (
-    await fetch(`${process.env.AUTH0_BASE_URL}/api/auth/me`, {
-      headers: { cookie: `appSession=${cookies().get('appSession')?.value}` },
-    })
-  ).text();
+import { getSession as auth0GetSession } from '@auth0/nextjs-auth0';
+import { IncomingMessage, ServerResponse } from 'http';
+import { Socket } from 'net';
+
+// https://github.com/auth0/nextjs-auth0/issues/889#issuecomment-1337896197
+// Note: This is an experiment to test that the SDK works in the experimental app directory.
+// You should not rely on this code (or the app directory) in production.
+const reqRes = () => {
+  const req = new IncomingMessage(new Socket());
+  const header = headers();
+  for (const key of header.keys()) {
+    req.headers[key] = header.get(key) || '';
+  }
+  return { req, res: new ServerResponse(req) };
+};
+
+const getSession = () => {
+  const { req, res } = reqRes();
+  return auth0GetSession(req, res);
+};
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const user = await getUser();
+  const session = await getSession();
   return (
     <html>
       <body>
-        <UserProvider user={user === '' ? undefined : JSON.parse(user)}>
-          {children}
-        </UserProvider>
+        <UserProvider user={session?.user}>{children}</UserProvider>
       </body>
     </html>
   );
