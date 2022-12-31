abc22b33d8599ff22e5207b3966f9926a5d47f3b
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-31 17:38:55 +0900

    ignore lint error during builds

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  eslint: { dirs: ['app', 'pages', 'lib', 'prisma'] },
+  experimental: { appDir: true },
+  eslint: { dirs: ['app', 'pages', 'lib', 'prisma'], ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
