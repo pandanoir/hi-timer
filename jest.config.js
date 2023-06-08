2ef0235671bd66d69c65392a050b3770c2be4578
pandanoir <moon_in_france@yahoo.co.jp>
2023-06-08 20:35:05 +0900

    move babel.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/__tests__/*'],
-  transform: { '^.+\\.[jt]sx?$': 'babel-jest' },
+  transform: {
+    '^.+\\.[jt]sx?$': [
+      'babel-jest',
+      { configFile: './__tests__/babel.config.js' },
+    ],
+  },
   transformIgnorePatterns: ['node_modules/(?!lizod)'],
   moduleNameMapper: { '\\.(css|less)$': '<rootDir>/__tests__/styleMock.ts' },
 };
