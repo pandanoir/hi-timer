521091fe6d901ccab42f1251c4661abf26221fbc
pandanoir <moon_in_france@yahoo.co.jp>
2023-11-18 19:38:50 +0900

    set initial color mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Metadata, Viewport } from 'next';
 import { setTimeout } from 'timers/promises';
 import { RootLayout } from './RootLayout';
 import { getSession } from './api/getSession';
+import { ColorModeScript } from '@chakra-ui/react';
+import { cookies } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Hi-Timer',
@@ -21,6 +23,13 @@ export default async function Layout({ children }: PropsWithChildren) {
   return (
     <html>
       <body>
+        <ColorModeScript
+          initialColorMode={
+            cookies().get('chakra-ui-color-mode')?.value === 'light'
+              ? 'light'
+              : 'dark'
+          }
+        />
         <RootLayout user={await Promise.race([userPromise, timeout])}>
           {children}
         </RootLayout>
