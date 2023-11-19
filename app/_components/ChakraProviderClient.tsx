8b6760dff26cc36f8f797fcfa2c4ffef243d5f03
pandanoir <moon_in_france@yahoo.co.jp>
2023-11-19 20:23:36 +0900

    refactor with cookieStorageManager

--- app/_components/ChakraProviderClient.tsx
+++ app/_components/ChakraProviderClient.tsx
@@ -1,9 +1,9 @@
 'use client';
 import {
   ChakraProvider,
-  ColorMode,
+  ColorModeScript,
+  cookieStorageManager,
   extendTheme,
-  localStorageManager,
 } from '@chakra-ui/react';
 import { PropsWithChildren } from 'react';
 
@@ -11,22 +11,12 @@ import { PropsWithChildren } from 'react';
 export const ChakraProviderClient = ({
   children,
   colorMode,
-}: PropsWithChildren<{ colorMode: 'light' | 'dark' | 'system' }>) => {
-  // HACK: cookie にも設定するために無理やり set メソッドを書き換えている
-  const colorModeManager = {
-    ...localStorageManager,
-    set(value: 'system' | ColorMode) {
-      document.cookie = `chakra-ui-color-mode=${value}; max-age=31536000; path=/`;
-      localStorageManager.set(value);
-    },
-  };
-
-  return (
-    <ChakraProvider
-      colorModeManager={colorModeManager}
-      theme={extendTheme({ config: { initialColorMode: colorMode } })}
-    >
-      {children}
-    </ChakraProvider>
-  );
-};
+}: PropsWithChildren<{ colorMode: 'light' | 'dark' | 'system' }>) => (
+  <ChakraProvider
+    colorModeManager={cookieStorageManager}
+    theme={extendTheme({ config: { initialColorMode: colorMode } })}
+  >
+    <ColorModeScript type="cookie" />
+    {children}
+  </ChakraProvider>
+);
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import { PropsWithChildren } from 'react';
 import { Metadata, Viewport } from 'next';
 import { setTimeout } from 'timers/promises';
 import { RootLayout } from './_components/RootLayout';
-import { ColorModeScript } from '@chakra-ui/react';
-import { cookies } from 'next/headers';
 import './global.css';
 import { getSession } from '@auth0/nextjs-auth0';
 
@@ -18,20 +16,12 @@ const viewport: Viewport = {
 export { viewport };
 
 export default async function Layout({ children }: PropsWithChildren) {
-  const cookieStore = cookies(); // HACK: getSession より後に cookies を呼び出すと DynamicServerError - Dynamic Server Usage で怒られる。そのためココで cookieStore に格納している
   const timeout = setTimeout(100, 'timeout' as const).then(() => undefined);
   const userPromise = getSession().then((session) => session?.user);
 
   return (
     <html>
       <body>
-        <ColorModeScript
-          initialColorMode={
-            cookieStore.get('chakra-ui-color-mode')?.value === 'light' ?
-              'light'
-            : 'dark'
-          }
-        />
         <RootLayout user={await Promise.race([userPromise, timeout])}>
           {children}
         </RootLayout>
