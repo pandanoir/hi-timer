a37bd2746b162370b664431f02d207904e6345ad
pandanoir <moon_in_france@yahoo.co.jp>
2023-11-20 22:11:20 +0900

    remove RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { Metadata, Viewport } from 'next';
 import { setTimeout } from 'timers/promises';
-import { RootLayout } from './_components/RootLayout';
 import './global.css';
 import { getSession } from '@auth0/nextjs-auth0';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { VStack } from '@chakra-ui/react';
+import { $enum } from 'lizod';
+import { cookies } from 'next/headers';
+import { ChakraProviderClient } from './_components/ChakraProviderClient';
+import { Header } from './_components/Header';
 
 export const metadata: Metadata = {
   title: 'Hi-Timer',
@@ -16,15 +21,29 @@ const viewport: Viewport = {
 export { viewport };
 
 export default async function Layout({ children }: PropsWithChildren) {
+  const initColorMode = cookies().get('chakra-ui-color-mode')?.value;
   const timeout = setTimeout(100, 'timeout' as const).then(() => undefined);
   const userPromise = getSession().then((session) => session?.user);
 
   return (
     <html>
       <body>
-        <RootLayout user={await Promise.race([userPromise, timeout])}>
-          {children}
-        </RootLayout>
+        <ChakraProviderClient
+          colorMode={
+            $enum(['light', 'dark'])(initColorMode) ? initColorMode : 'system'
+          }
+        >
+          <UserProvider user={await Promise.race([userPromise, timeout])}>
+            <VStack
+              align="left"
+              height="100dvh"
+              pb="env(safe-area-inset-bottom)"
+            >
+              <Header />
+              {children}
+            </VStack>
+          </UserProvider>
+        </ChakraProviderClient>
       </body>
     </html>
   );
